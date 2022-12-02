bd64a3c1f8262815c15055cf1dcceaf4c0e46d42
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-12-02 18:46:31 +0600

    update baseurl

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,26 +1,11 @@
 import axios from "axios";
 
 const config = {
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://habit-it.onrender.com/api",
+  // baseURL: "http://localhost:4000/api",
   timeout: 2000,
   withCredentials: true,
 };
 const axiosClient = axios.create(config);
 
 export default axiosClient;
-
-export function getRequest(URL) {
-  return axiosClient.get(`/${URL}`).then((response) => response);
-}
-
-export function postRequest(URL, payload) {
-  return axiosClient.post(`/${URL}`, payload).then((response) => response);
-}
-
-export function patchRequest(URL, payload) {
-  return axiosClient.patch(`/${URL}`, payload).then((response) => response);
-}
-
-export function deleteRequest(URL, payload) {
-  return axiosClient.delete(`${URL}`, payload).then((response) => response);
-}
