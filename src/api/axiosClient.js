78dfd0e9dcadb665cd0ac93734eac31998f7a945
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-12-02 19:16:40 +0600

    update baseurl

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const config = {
-  baseURL: "https://habit-it.onrender.com/api",
+  baseURL: "https://habitit.onrender.com/api",
   // baseURL: "http://localhost:4000/api",
   timeout: 2000,
   withCredentials: true,
