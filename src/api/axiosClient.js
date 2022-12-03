073320ab383b718d3764700b028802a434018e7f
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-12-03 07:00:18 +0600

    comment out timeout option of axios config

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const config = {
   baseURL: "https://habitit.onrender.com/api",
   // baseURL: "http://localhost:4000/api",
-  timeout: 2000,
+  // timeout: 2000,
   withCredentials: true,
 };
 const axiosClient = axios.create(config);
