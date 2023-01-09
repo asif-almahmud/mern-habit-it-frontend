34039f34f21e9ea858dfc8f0de38f59d3c15b75d
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-01-09 11:07:01 +0600

    make copyright year dynamic

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-   return (
-      <div className="border-t flex justify-center items-center h-16 text-sm tracking-wider">
-         <p>Copyright&copy; 2022, HABITit</p>
-      </div>
-   );
+    return (
+        <div className="border-t flex justify-center items-center h-16 text-sm tracking-wider">
+            <p>Copyright&copy; {new Date().getFullYear()}, HABITit</p>
+        </div>
+    );
 };
 
 export default Footer;
